fd1c6008a212db08fe2913f0cf28fabc8440bb03
jordanstark <jordan.r.stark@gmail.com>
2021-01-13 11:44:39 -0500

    added deployment dates

--- Prep_site_dat.R
+++ Prep_site_dat.R
@@ -2,15 +2,31 @@
 
 # libraries
 library(sp)
+library(lubridate)
 
 # import data
 sensordat <- read.csv("C:/Users/Jordan/Desktop/Smokies Data/SensorLocationHistory.csv")
 
 # clean site info
 
-sites <- sensordat[,c("SiteID","Watershed","X","Y")]
+sites <- sensordat[,c("SiteID","Watershed","X","Y","Deploy_Date","Remove_Date")]
+
+sites$Deploy_Date <- mdy(sites$Deploy_Date)
+sites$Remove_Date <- mdy(sites$Remove_Date)
+
+first_deploy <- aggregate(Deploy_Date ~ SiteID, sites, min, na.rm=T)
+last_remove  <- aggregate(Remove_Date ~ SiteID, sites, max, na.action="na.pass")
+
+sites$Deploy_Date <- NULL
+sites$Remove_Date <- NULL
+
 sites <- unique(sites)
+
 names(sites) <- c("SiteID","Watershed","UTM_E","UTM_N")
+
+sites <- merge(sites,first_deploy,all.x=T)
+sites <- merge(sites,last_remove,all.x=T)
+
 
 sites_sp <- SpatialPointsDataFrame(sites[,c("UTM_E","UTM_N")],data=sites,
                                    proj4string=CRS("+proj=utm +zone=17 +datum=WGS84 +units=m"))
